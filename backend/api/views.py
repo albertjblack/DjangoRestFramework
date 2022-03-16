3bc59f8e3053aa95426d097da9b5dd66f83444d0
albertjblack <albertjblack@outlook.com>
2022-03-16 17:05:56 -0600

    end echo get data, json passed in req is part of body

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+import json
+
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"hello there, this is Django Api Json Response"})
+
+    print(request.GET, " get") # will give query params from get request || backend side
+    print(request.POST, " post") # will give query params from post request || backend side
+
+    body = request.body
+    data = {} # || used in the client -> it is what json is returning
+    try:
+        data = json.loads(body) # takes a string of json and turns it into a dictionary
+    except Exception:
+        pass
+    
+    data["params"] = dict(request.GET)
+    data["headers"] = dict(request.headers)
+    data["content_type"] = request.content_type
+
+
+    return JsonResponse(data) 
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,6 @@ import requests
 
 # %% code
 endpoint = "http://localhost:8000/api/"
-get_response = requests.get(endpoint, json={"query":"Hello World"}) # API - using lib is like using an api // this is not rest api, cuz rest api is a web based, uses http requests
-print(get_response.status_code) # Http request -> HTML || REST Api http request -> JSON
-
+get_response = requests.get(endpoint, json={"query":"Hello World"}, params={"abc":"123"}) # API - using lib is like using an api // this is not rest api, cuz rest api is a web based, uses http requests
+print(get_response.json())
 
